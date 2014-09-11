c4143574da8a3043e9a2258816e09594904b3415
Peter Forgacs <peter.forgacs@spilgames.com>
2014-09-11 15:31:45 +0200

    Clean up

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,22 +1,16 @@
 package com.example.myfirstapp;
 
-//import android.support.v4.app.Fragment;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-//import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-//import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
@@ -24,20 +18,6 @@ public class DisplayMessageActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
-
-		/*
-	    // Get the message from the intent
-	    Intent intent = getIntent();
-	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-	    // Create the text view
-	    TextView textView = new TextView(this);
-	    textView.setTextSize(40);
-	    textView.setText(message);
-
-	    // Set the text view as the activity layout
-	    setContentView(textView);
-	    */
     }
 
 	@Override
@@ -58,22 +38,6 @@ public class DisplayMessageActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-	/*
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() { }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-              View rootView = inflater.inflate(R.layout.fragment_display_message, container, false);
-              return rootView;
-        }
-    }
-    */
 
     /**
      * A placeholder fragment containing a simple view. This fragment
--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -10,14 +10,10 @@ import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-	//public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		//EditText editText = (EditText) findViewById(R.id.edit_message);
-		//String message = editText.getText().toString();
-		//intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
 	
