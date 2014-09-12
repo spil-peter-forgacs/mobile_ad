22bbb8d81a473e8e0cb2edcc5c31fa632c2906c0
Peter Forgacs <peter.forgacs@spilgames.com>
2014-09-12 14:15:22 +0200

    Banner development

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,23 +1,100 @@
 package com.example.myfirstapp;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+//import com.google.example.gms.ads.banneradlistener.BannerAdListener;
+//import com.google.example.gms.ads.banneradlistener.R;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
+	private AdView adView;
+	private static final String AD_UNIT_ID = "ca-mb-app-pub-6675826664759732/6959510322";	  
+
+	/** The log tag. */
+	private static final String LOG_TAG = "BannerAdListener";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
+		
+	    // Create an ad.
+	    adView = new AdView(this);
+	    adView.setAdSize(AdSize.BANNER);
+	    adView.setAdUnitId(AD_UNIT_ID);
+
+	    // Set the AdListener.
+	    adView.setAdListener(new AdListener() {
+	      /** Called when an ad is clicked and about to return to the application. */
+	      @Override
+	      public void onAdClosed() {
+	        Log.d(LOG_TAG, "onAdClosed");
+	        Toast.makeText(DisplayMessageActivity.this, "onAdClosed", Toast.LENGTH_SHORT).show();
+	      }
+
+	      /** Called when an ad failed to load. */
+	      @Override
+	      public void onAdFailedToLoad(int error) {
+	        String message = "onAdFailedToLoad: " + getErrorReason(error);
+	        Log.d(LOG_TAG, message);
+	        Toast.makeText(DisplayMessageActivity.this, message, Toast.LENGTH_SHORT).show();
+	      }
+
+	      /**
+	       * Called when an ad is clicked and going to start a new Activity that will
+	       * leave the application (e.g. breaking out to the Browser or Maps
+	       * application).
+	       */
+	      @Override
+	      public void onAdLeftApplication() {
+	        Log.d(LOG_TAG, "onAdLeftApplication");
+	        Toast.makeText(DisplayMessageActivity.this, "onAdLeftApplication", Toast.LENGTH_SHORT).show();
+	      }
+
+	      /**
+	       * Called when an Activity is created in front of the app (e.g. an
+	       * interstitial is shown, or an ad is clicked and launches a new Activity).
+	       */
+	      @Override
+	      public void onAdOpened() {
+	        Log.d(LOG_TAG, "onAdOpened");
+	        Toast.makeText(DisplayMessageActivity.this, "onAdOpened", Toast.LENGTH_SHORT).show();
+	      }
+
+	      /** Called when an ad is loaded. */
+	      @Override
+	      public void onAdLoaded() {
+	        Log.d(LOG_TAG, "onAdLoaded");
+	        Toast.makeText(DisplayMessageActivity.this, "onAdLoaded", Toast.LENGTH_SHORT).show();
+	      }
+	    });    
+	    // Add the AdView to the view hierarchy.
+	    LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+	    layout.addView(adView);
+
+	    // Create an ad request. Check logcat output for the hashed device ID to
+	    // get test ads on a physical device.
+	    AdRequest adRequest = new AdRequest.Builder()
+	        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .addTestDevice("C7780C08B0538F034D630D3AC89DF5E8")
+	        .build();
+
+	    // Start loading the ad in the background.
+	    adView.loadAd(adRequest);
     }
 
 	@Override
@@ -38,85 +115,24 @@ public class DisplayMessageActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-    /**
-     * A placeholder fragment containing a simple view. This fragment
-     * would include your content.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-            return rootView;
-        }
-    }
-
-    /**
-     * This class makes the ad request and loads the ad.
-     */
-    public static class AdFragment extends Fragment {
-
-        private AdView mAdView;
-
-        public AdFragment() {
-        }
-
-        @Override
-        public void onActivityCreated(Bundle bundle) {
-            super.onActivityCreated(bundle);
-
-            // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-            // values/strings.xml.
-            mAdView = (AdView) getView().findViewById(R.id.adView);
-
-            // Create an ad request. Check logcat output for the hashed device ID to
-            // get test ads on a physical device. e.g.
-            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-            AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                    .build();
-
-            // Start loading the ad in the background.
-            mAdView.loadAd(adRequest);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_ad, container, false);
-        }
-
-        /** Called when leaving the activity */
-        @Override
-        public void onPause() {
-            if (mAdView != null) {
-                mAdView.pause();
-            }
-            super.onPause();
-        }
-
-        /** Called when returning to the activity */
-        @Override
-        public void onResume() {
-            super.onResume();
-            if (mAdView != null) {
-                mAdView.resume();
-            }
-        }
-
-        /** Called before the activity is destroyed */
-        @Override
-        public void onDestroy() {
-            if (mAdView != null) {
-                mAdView.destroy();
-            }
-            super.onDestroy();
-        }
-
+	  
+    /** Gets a string error reason from an error code. */
+    private String getErrorReason(int errorCode) {
+      String errorReason = "";
+      switch(errorCode) {
+        case AdRequest.ERROR_CODE_INTERNAL_ERROR:
+          errorReason = "Internal error";
+          break;
+        case AdRequest.ERROR_CODE_INVALID_REQUEST:
+          errorReason = "Invalid request";
+          break;
+        case AdRequest.ERROR_CODE_NETWORK_ERROR:
+          errorReason = "Network Error";
+          break;
+        case AdRequest.ERROR_CODE_NO_FILL:
+          errorReason = "No fill";
+          break;
+      }
+      return errorReason;
     }
 }
